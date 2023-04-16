a486abe6fab862aba1d1b8ea202b836f8d6e14ee
bill-32 <114152973+bill-32@users.noreply.github.com>
2023-04-16 15:00:02 +0800

    Update bingo.cpp

--- 955209/bingo.cpp
+++ 955209/bingo.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main() {
   srand(time(0));
-  int bingo[5][5], number[26]={0}, answer[5];
+  int bingo[5][5], number[26]={0}, number2[26]={0}, answer[26], line=0;
   for(int i=0 ; i<5 ; i++){
     for(int j=0 ; j<5 ; j++){
       int pick;
@@ -21,9 +21,65 @@ int main() {
     }
     cout << "\n";
   }
-  for(int i=0;i<5;i++){
-    answer[i]=rand()%25+1;
+  cout << "\n";
+  for(int i=0;i<20;i++){
+    int pick;
+    do{
+      pick = rand()%25+1;
+    }while(number2[pick]>0);
+    number2[pick]++;
+    answer[i] = pick ;
     cout.width(3);
-    cout<<answer[i]<<" ";
+    cout << pick << ",";
   }
+  cout << "\n" << "\n";
+  for(int a=0;a<20;a++){
+    for(int i=0 ; i<5 ; i++){
+      for(int j=0 ; j<5 ; j++){
+        if(answer[a]==bingo[i][j])bingo [i][j]=0;
+     }
+   }
+  }
+  for(int i=0 ; i<5 ; i++){
+    for(int j=0 ; j<5 ; j++){
+      cout.width(3);
+      cout << bingo [i][j] << ",";
+    }
+    cout << "\n";
+  }
+  cout << "\n";
+  for(int i=0;i<5;i++){
+    bool flag=true, flag2=true;
+    for(int j=0;j<5;j++){
+      if(bingo[i][j]!=0){
+        flag=false;
+      }
+      if(bingo[j][i]!=0){
+        flag2=false;
+      }
+    }
+    if(flag==true){
+      line++; 
+    }
+    if(flag2==true){
+      line++; 
+    }
+  }
+  bool flag=true, flag2=true;
+  for(int j=0 ; j<5 ; j++){
+    if(bingo[j][j]!=0){
+      flag=false;
+    }
+    if(bingo[4-j][j]!=0){
+      flag2=false;
+    }
+  }
+  if(flag==true){
+    line++; 
+  }
+  if(flag2==true){
+    line++; 
+  }
+  cout << line;
+  return 0;
 }
